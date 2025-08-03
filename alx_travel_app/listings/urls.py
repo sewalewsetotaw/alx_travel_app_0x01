e7cf4be84f29f151a21ac0d23f8e53eb5b22a7a5
SewalewS29 <sewalews29@gmail.com>
2025-08-03 03:11:22 +0300

    add CRUD Operation API

--- alx_travel_app/listings/urls.py
+++ alx_travel_app/listings/urls.py
@@ -1,7 +1,14 @@
 # listings/urls.py
-from django.urls import path
-from .views import SampleAPIView
+
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import ListingViewSet, BookingViewSet, SampleAPIView
+
+router = DefaultRouter()
+router.register(r'listings', ListingViewSet)
+router.register(r'bookings', BookingViewSet)
 
 urlpatterns = [
+    path('', include(router.urls)),
     path('sample/', SampleAPIView.as_view()),
 ]
--- alx_travel_app/listings/views.py
+++ alx_travel_app/listings/views.py
@@ -2,7 +2,16 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from .serializers import ListingSerializer, BookingSerializer
+from rest_framework import viewsets, filters, status
+from .models import Listing,Booking
+class ListingViewSet(viewsets.ModelViewSet):
+    queryset = Listing.objects.all()
+    serializer_class = ListingSerializer
 
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
 class SampleAPIView(APIView):
     def get(self, request):
         return Response({"message": "Welcome to ALX Travel App"})
